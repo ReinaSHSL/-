720e1f0767cc2adeebaa3c46c728fe29c4c5790e
reina <ReinaSHSL@users.noreply.github.com>
2019-03-23 16:52:16 -0700

    formatting

--- whoareyou/src/main/java/questionableDecisions/characters/PUTMEOUTOFMYMISERY.java
+++ whoareyou/src/main/java/questionableDecisions/characters/PUTMEOUTOFMYMISERY.java
@@ -29,33 +29,21 @@ import java.util.ArrayList;
 import static questionableDecisions.MORECHAOSMOREPOWER.*;
 import static questionableDecisions.characters.PUTMEOUTOFMYMISERY.Enums.COLOR_GRAY;
 
-//Wiki-page https://github.com/daviscook477/BaseMod/wiki/Custom-Characters
-//and https://github.com/daviscook477/BaseMod/wiki/Migrating-to-5.0
-//All text (starting description and loadout, anything labeled TEXT[]) can be found in MORECHAOSMOREPOWER-character-Strings.json in the resources
 
 public class PUTMEOUTOFMYMISERY extends CustomPlayer {
     public static final Logger logger = LogManager.getLogger(MORECHAOSMOREPOWER.class.getName());
 
-    // =============== CHARACTER ENUMERATORS =================
-    // These are enums for your Characters color (both general color and for the card library) as well as
-    // an enum for the name of the player class - IRONCLAD, THE_SILENT, DEFECT, YOUR_CLASS ...
-    // These are all necessary for creating a character. If you want to find out where and how exactly they are used
-    // in the basegame (for fun and education) Ctrl+click on the PlayerClass, CardColor and/or LibraryType below and go down the
-    // Ctrl+click rabbit hole
 
     public static class Enums {
         @SpireEnum
         public static AbstractPlayer.PlayerClass AAAAAAAAA;
-        @SpireEnum(name = "DEFAULT_GRAY_COLOR") // These two HAVE to have the same absolutely identical name.
+        @SpireEnum(name = "DEFAULT_GRAY_COLOR")
         public static AbstractCard.CardColor COLOR_GRAY;
-        @SpireEnum(name = "DEFAULT_GRAY_COLOR") @SuppressWarnings("unused")
+        @SpireEnum(name = "DEFAULT_GRAY_COLOR")
+        @SuppressWarnings("unused")
         public static CardLibrary.LibraryType LIBRARY_COLOR;
     }
 
-    // =============== CHARACTER ENUMERATORS  =================
-
-
-    // =============== BASE STATS =================
 
     public static final int ENERGY_PER_TURN = 3;
     public static final int STARTING_HP = 75;
@@ -64,20 +52,12 @@ public class PUTMEOUTOFMYMISERY extends CustomPlayer {
     public static final int CARD_DRAW = 9;
     public static final int ORB_SLOTS = 3;
 
-    // =============== /BASE STATS/ =================
-
-
-    // =============== STRINGS =================
 
     private static final String ID = makeID("???");
     private static final CharacterStrings characterStrings = CardCrawlGame.languagePack.getCharacterString(ID);
     private static final String[] NAMES = characterStrings.NAMES;
     private static final String[] TEXT = characterStrings.TEXT;
 
-    // =============== /STRINGS/ =================
-
-
-    // =============== TEXTURES OF BIG ENERGY ORB ===============
 
     public static final String[] orbTextures = {
             "questionableDecisionsResources/images/char/defaultCharacter/orb/layer1.png",
@@ -92,9 +72,6 @@ public class PUTMEOUTOFMYMISERY extends CustomPlayer {
             "questionableDecisionsResources/images/char/defaultCharacter/orb/layer4d.png",
             "questionableDecisionsResources/images/char/defaultCharacter/orb/layer5d.png",};
 
-    // =============== /TEXTURES OF BIG ENERGY ORB/ ===============
-
-    // =============== CHARACTER CLASS START =================
 
     public PUTMEOUTOFMYMISERY(String name, PlayerClass setClass) {
         super(name, setClass, orbTextures,
@@ -103,19 +80,13 @@ public class PUTMEOUTOFMYMISERY extends CustomPlayer {
                         "questionableDecisionsResources/images/char/defaultCharacter/Spriter/theDefaultAnimation.scml"));
 
 
-        // =============== TEXTURES, ENERGY, LOADOUT =================  
+        initializeClass(null,
 
-        initializeClass(null, // required call to load textures and setup energy/loadout.
-                // I left these in MORECHAOSMOREPOWER.java (Ctrl+click them to see where they are, Ctrl+hover to see what they read.)
-                THE_DEFAULT_SHOULDER_1, // campfire pose
-                THE_DEFAULT_SHOULDER_2, // another campfire pose
-                THE_DEFAULT_CORPSE, // dead corpse
-                getLoadout(), 20.0F, -10.0F, 220.0F, 290.0F, new EnergyManager(ENERGY_PER_TURN)); // energy manager
+                THE_DEFAULT_SHOULDER_1,
+                THE_DEFAULT_SHOULDER_2,
+                THE_DEFAULT_CORPSE,
+                getLoadout(), 20.0F, -10.0F, 220.0F, 290.0F, new EnergyManager(ENERGY_PER_TURN));
 
-        // =============== /TEXTURES, ENERGY, LOADOUT/ =================
-
-
-        // =============== ANIMATIONS =================  
 
         loadAnimation(
                 THE_DEFAULT_SKELETON_ATLAS,
@@ -124,21 +95,14 @@ public class PUTMEOUTOFMYMISERY extends CustomPlayer {
         AnimationState.TrackEntry e = state.setAnimation(0, "animation", true);
         e.setTime(e.getEndTime() * MathUtils.random());
 
-        // =============== /ANIMATIONS/ =================
 
+        dialogX = (drawX + 0.0F * Settings.scale);
+        dialogY = (drawY + 220.0F * Settings.scale);
 
-        // =============== TEXT BUBBLE LOCATION =================
-
-        dialogX = (drawX + 0.0F * Settings.scale); // set location for text bubbles
-        dialogY = (drawY + 220.0F * Settings.scale); // you can just copy these values
-
-        // =============== /TEXT BUBBLE LOCATION/ =================
 
     }
 
-    // =============== /CHARACTER CLASS END/ =================
 
-    // Starting description and loadout
     @Override
     public CharSelectInfo getLoadout() {
         return new CharSelectInfo(NAMES[0], TEXT[0],
@@ -146,7 +110,7 @@ public class PUTMEOUTOFMYMISERY extends CustomPlayer {
                 getStartingDeck(), false);
     }
 
-    // Starting Deck
+
     @Override
     public ArrayList<String> getStartingDeck() {
         ArrayList<String> retVal = new ArrayList<>();
@@ -158,7 +122,7 @@ public class PUTMEOUTOFMYMISERY extends CustomPlayer {
         return retVal;
     }
 
-    // Starting Relics	
+
     public ArrayList<String> getStartingRelics() {
         ArrayList<String> retVal = new ArrayList<>();
 
@@ -166,86 +130,81 @@ public class PUTMEOUTOFMYMISERY extends CustomPlayer {
         return retVal;
     }
 
-    // character Select screen effect
+
     @Override
     public void doCharSelectScreenSelectEffect() {
-        CardCrawlGame.sound.playA("ATTACK_DAGGER_1", 1.25f); // Sound Effect
+        CardCrawlGame.sound.playA("ATTACK_DAGGER_1", 1.25f);
         CardCrawlGame.screenShake.shake(ScreenShake.ShakeIntensity.LOW, ScreenShake.ShakeDur.SHORT,
-                false); // Screen Effect
+                false);
     }
 
-    // character Select on-button-press sound effect
+
     @Override
     public String getCustomModeCharacterButtonSoundKey() {
         return "ATTACK_DAGGER_1";
     }
 
-    // Should return how much HP your maximum HP reduces by when starting a run at
-    // Ascension 14 or higher. (ironclad loses 5, defect and silent lose 4 hp respectively)
+
     @Override
     public int getAscensionMaxHPLoss() {
         return 0;
     }
 
-    // Should return the card color enum to be associated with your character.
+
     @Override
     public AbstractCard.CardColor getCardColor() {
         return COLOR_GRAY;
     }
 
-    // Should return a color object to be used to color the trail of moving cards
+
     @Override
     public Color getCardTrailColor() {
         return MORECHAOSMOREPOWER.DEFAULT_GRAY;
     }
 
-    // Should return a BitmapFont object that you can use to customize how your
-    // energy is displayed from within the energy orb.
+
     @Override
     public BitmapFont getEnergyNumFont() {
         return FontHelper.energyNumFontRed;
     }
 
-    // Should return class name as it appears in run history screen.
+
     @Override
     public String getLocalizedCharacterName() {
         return NAMES[0];
     }
 
-    //Which card should be obtainable from the Match and Keep event?
+
     @Override
     public AbstractCard getStartCardForEvent() {
         return new Strike_Red();
     }
 
-    // The class name as it appears next to your player name in-game
+
     @Override
     public String getTitle(AbstractPlayer.PlayerClass playerClass) {
         return NAMES[1];
     }
 
-    // Should return a new instance of your character, sending name as its name parameter.
+
     @Override
     public AbstractPlayer newInstance() {
         return new PUTMEOUTOFMYMISERY(name, chosenClass);
     }
 
-    // Should return a Color object to be used to color the miniature card images in run history.
+
     @Override
     public Color getCardRenderColor() {
         return MORECHAOSMOREPOWER.DEFAULT_GRAY;
     }
 
-    // Should return a Color object to be used as screen tint effect when your
-    // character attacks the heart.
+
     @Override
     public Color getSlashAttackColor() {
         return MORECHAOSMOREPOWER.DEFAULT_GRAY;
     }
 
-    // Should return an AttackEffect array of any size greater than 0. These effects
-    // will be played in sequence as your character's finishing combo on the heart.
-    // Attack effects are the same as used in DamageAction and the like.
+
     @Override
     public AbstractGameAction.AttackEffect[] getSpireHeartSlashEffect() {
         return new AbstractGameAction.AttackEffect[]{
@@ -254,17 +213,13 @@ public class PUTMEOUTOFMYMISERY extends CustomPlayer {
                 AbstractGameAction.AttackEffect.BLUNT_HEAVY};
     }
 
-    // Should return a string containing what text is shown when your character is
-    // about to attack the heart. For example, the defect is "NL You charge your
-    // core to its maximum..."
+
     @Override
     public String getSpireHeartText() {
         return TEXT[1];
     }
 
-    // The vampire events refer to the base game characters as "brother", "sister",
-    // and "broken one" respectively.This method should return a String containing
-    // the full text that will be displayed as the first screen of the vampires event.
+
     @Override
     public String getVampireText() {
         return TEXT[2];
