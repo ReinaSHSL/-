51ec2f7a776f37e8beb27238629a94b118e1e87e
reina <ReinaSHSL@users.noreply.github.com>
2019-03-23 16:48:37 -0700

    rotateo

--- whoareyou/src/main/java/questionableDecisions/patches/CharSelectScreenPatch.java
+++ whoareyou/src/main/java/questionableDecisions/patches/CharSelectScreenPatch.java
@@ -1,6 +1,7 @@
 package questionableDecisions.patches;
 
 import basemod.BaseMod;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -23,18 +24,25 @@ public class CharSelectScreenPatch {
     private static Random r = new Random();
     private static ArrayList<String> characterPortraits = new ArrayList<>();
     private static int rng;
+    private static float waitTimer = 0.25f;
     public static void Prefix(CharacterSelectScreen __instance, SpriteBatch sb) {
         if (characterPortraits.isEmpty()) {
             for (String s : BaseMod.playerPortraitMap.values()) {
                 characterPortraits.add(s);
             }
+            characterPortraits.add("images/ui/charSelect/ironcladPortrait.jpg");
+            characterPortraits.add("images/ui/charSelect/silentPortrait.jpg");
+            characterPortraits.add("images/ui/charSelect/defectPortrait.jpg");
         }
         for (CharacterOption o: __instance.options) {
             if (o.selected && o.c instanceof PUTMEOUTOFMYMISERY) {
-               rng = r.nextInt((characterPortraits.size() - 1 + 0) + 1);
-                __instance.bgCharImg = TextureLoader.getTexture(characterPortraits.get(rng));
+                waitTimer -= Gdx.graphics.getDeltaTime();
+                if (waitTimer <= 0) {
+                    rng = r.nextInt((characterPortraits.size() - 1 + 0) + 1);
+                    __instance.bgCharImg = TextureLoader.getTexture(characterPortraits.get(rng));
+                    waitTimer = 0.25f;
+                }
             }
         }
-
     }
 }
