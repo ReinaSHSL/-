07929fd35d47dcc55a6773c68e9ca1444e7f129f
reina <ReinaSHSL@users.noreply.github.com>
2019-03-23 19:39:56 -0700

    make generate card function

--- whoareyou/src/main/java/questionableDecisions/MORECHAOSMOREPOWER.java
+++ whoareyou/src/main/java/questionableDecisions/MORECHAOSMOREPOWER.java
@@ -10,10 +10,13 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
 import com.google.gson.Gson;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.helpers.CardHelper;
 import com.megacrit.cardcrawl.localization.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import questionableDecisions.cards.AbstractWtfCard;
 import questionableDecisions.characters.PUTMEOUTOFMYMISERY;
 import questionableDecisions.util.IDCheckDontTouchPls;
 import questionableDecisions.util.TextureLoader;
@@ -21,6 +24,8 @@ import questionableDecisions.util.TextureLoader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
 
 @SpireInitializer
 public class MORECHAOSMOREPOWER implements
@@ -308,4 +313,88 @@ public class MORECHAOSMOREPOWER implements
         return getModID() + ":" + idText;
     }
 
+    public static AbstractCard generateCard() {
+        ArrayList<Components> allPossibleCardDesc = new ArrayList<>();
+        for (MORECHAOSMOREPOWER.Components cd : MORECHAOSMOREPOWER.Components.values()) {
+            if (!cd.name().startsWith("WHEN_")) {
+                allPossibleCardDesc.add(cd);
+            }
+        }
+        Collections.shuffle(allPossibleCardDesc);
+        int weight = 80;
+        int loseWeight = 30;
+
+        int cost = AbstractDungeon.cardRng.random(3);
+        int d = getDamage(cost);
+        int b = getBlock(cost);
+        int magic = getMagic(cost);
+        int wtfMagic = getMagic(cost);
+        int du = AbstractDungeon.cardRng.random(3, 5);
+        int bu = AbstractDungeon.cardRng.random(3, 5);
+        int cu = AbstractDungeon.cardRng.random(cost);
+        int mu = AbstractDungeon.cardRng.random(0, 1);
+        int muu = AbstractDungeon.cardRng.random(0, 1);
+
+        AbstractWtfCard c = new AbstractWtfCard("",cost, AbstractCard.CardRarity.SPECIAL, cu, du, bu, mu, muu, d, b, magic, wtfMagic) {
+        };
+
+        for (MORECHAOSMOREPOWER.Components de : allPossibleCardDesc) {
+            int rng = AbstractDungeon.cardRng.random(99);
+            if (rng < weight) {
+                c.componentList.add(de);
+                weight -= loseWeight;
+                if (weight <= 0) {
+                    weight = 5;
+                }
+            }
+        }
+        c.setCardInfo();
+        c.resetAttributes();
+        c.magicNumber = c.baseMagicNumber;
+        c.wtfMagicNumber = c.baseWtfMagicNumber;
+        c.buildID();
+        return c;
+    }
+
+    private static int getDamage(int cost) {
+        switch (cost) {
+            case 0:
+                return AbstractDungeon.cardRng.random(3, 5);
+            case 1:
+                return AbstractDungeon.cardRng.random(9, 12);
+            case 2:
+                return AbstractDungeon.cardRng.random(17, 24);
+            case 3:
+                return AbstractDungeon.cardRng.random(26, 36);
+        }
+        return 0;
+    }
+
+    private static int getBlock(int cost) {
+        switch (cost) {
+            case 0:
+                return AbstractDungeon.cardRng.random(3, 5);
+            case 1:
+                return AbstractDungeon.cardRng.random(7, 9);
+            case 2:
+                return AbstractDungeon.cardRng.random(14, 20);
+            case 3:
+                return AbstractDungeon.cardRng.random(24, 36);
+        }
+        return 0;
+    }
+
+    private static int getMagic(int cost) {
+        switch (cost) {
+            case 0:
+                return AbstractDungeon.cardRng.random(1, 1);
+            case 1:
+                return AbstractDungeon.cardRng.random(1, 2);
+            case 2:
+                return AbstractDungeon.cardRng.random(2, 3);
+            case 3:
+                return AbstractDungeon.cardRng.random(3, 5);
+        }
+        return 0;
+    }
 }
