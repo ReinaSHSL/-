6b6b5883e775b1baf97faed56627be749cbee8f3
reina <ReinaSHSL@users.noreply.github.com>
2019-03-23 19:25:09 -0700

    del for now

--- whoareyou/src/main/java/questionableDecisions/patches/TurnsOutMakingCardsThatOnlyExistAtRuntimeKindaSucksPatches.java
+++ whoareyou/src/main/java/questionableDecisions/patches/TurnsOutMakingCardsThatOnlyExistAtRuntimeKindaSucksPatches.java
@@ -8,41 +8,4 @@ import questionableDecisions.cards.AbstractWtfCard;
 
 public class TurnsOutMakingCardsThatOnlyExistAtRuntimeKindaSucksPatches {
 
-    @SpirePatch(
-            clz = CardLibrary.class,
-            method = "getCopy",
-            paramtypez = {
-                    String.class,
-                    String.class,
-                    int.class
-            }
-    )
-    public static class GetCopyAndUpgradePatch {
-        public static SpireReturn Prefix(String id, int upgrades, int misc) {
-            AbstractCard cardToFind = CardLibrary.cards.get(id);
-            if (cardToFind == null) {
-                AbstractWtfCard.makeCopy(id, upgrades, misc);
-                return SpireReturn.Return(null);
-            }
-            return SpireReturn.Continue();
-        }
-    }
-
-    @SpirePatch(
-            clz = CardLibrary.class,
-            method = "getCopy",
-            paramtypez = {
-                    String.class,
-            }
-    )
-    public static class GetCopyAndUpgradePatchSingleParam {
-        public static SpireReturn Prefix(String id) {
-            AbstractCard cardToFind = CardLibrary.cards.get(id);
-            if (cardToFind == null) {
-                AbstractWtfCard.makeCopy(id);
-                return SpireReturn.Return(null);
-            }
-            return SpireReturn.Continue();
-        }
-    }
 }
